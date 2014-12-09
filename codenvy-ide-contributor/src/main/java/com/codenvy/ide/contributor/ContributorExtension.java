7c63b691c1498f581051f7ccd91a1ae14548316f
Stéphane Tournié <stephane.tournie@serli.com>
2014-12-09 14:01:56 +0100

    correcting extension name & adding some basic content

--- codenvy-ide-contributor/src/main/java/com/codenvy/ide/contributor/ContributorExtension.java
+++ codenvy-ide-contributor/src/main/java/com/codenvy/ide/contributor/ContributorExtension.java
@@ -10,20 +10,54 @@
  *******************************************************************************/
 package com.codenvy.ide.contributor;
 
+import java.util.List;
+import java.util.Map;
+
+import com.codenvy.api.project.gwt.client.ProjectServiceClient;
+import com.codenvy.ide.api.event.ProjectActionEvent;
+import com.codenvy.ide.api.event.ProjectActionHandler;
 import com.codenvy.ide.api.extension.Extension;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.google.web.bindery.event.shared.EventBus;
 
 /**
  * @author 
  */
 @Singleton
-@Extension(title = "Factory", version = "3.0.0")
+@Extension(title = "Contributor", version = "1.0.0")
 public class ContributorExtension {
     
     @Inject
-    public ContributorExtension() {
+    public ContributorExtension(EventBus eventBus, ProjectServiceClient projectServiceClient) {
         
+        eventBus.addHandler(ProjectActionEvent.TYPE, new ProjectActionHandler() {
+            @Override
+            public void onProjectOpened(ProjectActionEvent event) {
+                loadData(event);
+            }
+
+            @Override
+            public void onProjectClosed(ProjectActionEvent projectActionEvent) {
+
+            }
+        });
     }
-                           
+
+    /**
+     * Load the data for the given project
+     *
+     * @param event
+     *         the load event
+     */
+    protected void loadData(ProjectActionEvent event) {
+
+        Map<String, List<String>> attributes = event.getProject().getAttributes();
+
+        if (attributes != null && attributes.containsKey("attributes")) {
+            String projectTypeAttributes = attributes.get("attributes").get(0);
+        } else {
+
+        }
+    }
 }
