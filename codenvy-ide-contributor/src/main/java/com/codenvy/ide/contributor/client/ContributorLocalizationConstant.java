6db9a3298c95ff045461a0cbaab2f70f146a290e
Stéphane Tournié <stephane.tournie@serli.com>
2014-12-10 09:56:51 +0100

    adding constant class & properties

---
+++ codenvy-ide-contributor/src/main/java/com/codenvy/ide/contributor/client/ContributorLocalizationConstant.java
@@ -0,0 +1,12 @@
+package com.codenvy.ide.contributor.client;
+
+import com.google.gwt.i18n.client.Messages;
+
+public interface ContributorLocalizationConstant extends Messages {
+
+    @Key("contributor.button.name")
+    String contributorButtonName();
+    
+    @Key("contributor.button.title")
+    String contributorButtonTitle();
+}
