42b6ab1042f14acc646418971929ce55366e2fd2
Mickaël Leduque <mickael.leduque@serli.com>
2014-12-12 15:34:08 +0100

    Define repository host dto

---
+++ codenvy-plugin-contribution/src/main/java/com/codenvy/plugin/contribution/client/vcshost/HostUser.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package com.codenvy.plugin.contribution.client.vcshost;
+
+import com.codenvy.dto.shared.DTO;
+
+@DTO
+public interface HostUser {
+    String getId();
+
+    void setId(String id);
+
+    HostUser withId(String id);
+
+    String getName();
+
+    void setName(String name);
+
+    HostUser withName(String name);
+
+    String getLogin();
+
+    void setLogin(String login);
+
+    HostUser withLogin(String login);
+
+    String getUrl();
+
+    void setUrl(String url);
+
+    HostUser withUrl(String url);
+}
---
+++ codenvy-plugin-contribution/src/main/java/com/codenvy/plugin/contribution/client/vcshost/Repository.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package com.codenvy.plugin.contribution.client.vcshost;
+
+public interface Repository {
+    String getName();
+
+    void setName(String name);
+
+    Repository withName(String name);
+
+    String getUrl();
+
+    void setUrl(String url);
+
+    Repository withUrl(String url);
+
+    boolean isFork();
+
+    void setFork(boolean isFork);
+
+    Repository withFork(boolean isFork);
+
+    boolean isPrivateRepo();
+
+    void setPrivateRepo(boolean isPrivateRepo);
+
+    Repository withPrivateRepo(boolean isPrivateRepo);
+}
