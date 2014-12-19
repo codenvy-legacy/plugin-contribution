dd3a99f61f4e14d348fb5788e795c30c96857913
Mickaël Leduque <mickael.leduque@serli.com>
2014-12-19 19:51:19 +0100

    JSO does not permit multiple JSO implementation of the same interface

--- codenvy-plugin-contribution/src/main/java/com/codenvy/plugin/contribution/client/jso/File.java
+++ codenvy-plugin-contribution/src/main/java/com/codenvy/plugin/contribution/client/jso/File.java
@@ -15,7 +15,30 @@ import java.util.Date;
 /**
  * Interface for FileAPI File type.
  */
-public interface File extends Blob {
+public interface File {
+
+    /**
+     * Returns the size of the Blob object in bytes. On getting, conforming user agents must return<br>
+     * the total number of bytes that can be read by a FileReader or FileReaderSync object, or 0 if<br>
+     * the Blob has no bytes to be read. If the Blob has been neutered with close called on it, then<br>
+     * size must return 0.
+     * 
+     * @return the size
+     */
+    double getSize();
+
+    /**
+     * Returns The ASCII-encoded string in lower case representing the media type of the Blob. For File<br>
+     * objects that are returned from the underlying file system, user agents must return the type of a<br>
+     * Blob as an ASCII-encoded string in lower case, such that when it is converted to a corresponding<br>
+     * byte sequence, it is a parsable MIME type [MIMESNIFF], or the empty string -- 0 bytes -- if the<br>
+     * type cannot be determined. When the Blob is of type text/plain user agents must NOT append a charset<br>
+     * parameter to the dictionary of parameters portion of the media type [MIMESNIFF]. User agents must not<br>
+     * attempt heuristic determination of encoding, including statistical methods.
+     * 
+     * @return the media type
+     */
+    String getType();
 
     /**
      * Retuns the last modified date of the file. On getting, if user agents can make this information available,<br>
