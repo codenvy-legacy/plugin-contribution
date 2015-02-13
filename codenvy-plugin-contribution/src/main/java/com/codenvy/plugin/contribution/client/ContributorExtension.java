aa9892aa9f1af710865b1b6a5913923c9ba32e5f
Evgen Vidolob <evidolob@codenvy.com>
2015-02-13 15:26:26 +0200

    IDEX-2016 copy mixin types, fix contibution plugin

--- codenvy-plugin-contribution/src/main/java/com/codenvy/plugin/contribution/client/ContributorExtension.java
+++ codenvy-plugin-contribution/src/main/java/com/codenvy/plugin/contribution/client/ContributorExtension.java
@@ -204,6 +204,7 @@ public class ContributorExtension implements ProjectActionHandler {
         projectUpdate.setDescription(projectDescriptor.getDescription());
         projectUpdate.setAttributes(projectDescriptor.getAttributes());
         projectUpdate.setRunners(projectDescriptor.getRunners());
+        projectUpdate.setMixinTypes(projectDescriptor.getMixins());
         projectUpdate.setBuilders(projectDescriptor.getBuilders());
     }
 }
